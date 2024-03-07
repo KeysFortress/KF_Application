8ffbe6e188a7a65b333dcf4181a6b2e96ff0bcf7
Kristifor Milchev <km@airzen.io>
2024-03-07 16:52:52 +0200

    Replacing the placeholder exception panel with a new themed in the current application style and colors.

--- lib/implementations/exception_manager.dart
+++ lib/implementations/exception_manager.dart
@@ -3,6 +3,7 @@ import 'package:domain/exceptions/base_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:infrastructure/interfaces/iexception_manager.dart';
 import 'package:infrastructure/interfaces/ilogging_service.dart';
+import 'package:domain/styles.dart';
 
 class ExceptionManager implements IExceptionManager {
   Flushbar? _activeBar;
@@ -24,70 +25,52 @@ class ExceptionManager implements IExceptionManager {
     }
 
     _activeBar ??= Flushbar(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(20),
-        topRight: Radius.circular(20),
-      ),
       onStatusChanged: (status) => barStatusChanged(status),
       padding: const EdgeInsets.all(0),
       messageText: Material(
         color: Colors.transparent,
         child: Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-            color: Colors.black,
-          ),
           padding: const EdgeInsets.all(0),
           margin: const EdgeInsets.all(0),
-          child: Container(
-            padding: const EdgeInsets.all(0),
-            margin: const EdgeInsets.all(0),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(192, 192, 192, 0.3),
-                  Color.fromRGBO(192, 192, 192, 0),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  exception.title ?? "",
-                  style: const TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    letterSpacing: 2,
-                    fontSize: 20,
-                    fontFamily: "Loto",
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.center,
+          color: ThemeStyles.theme.background200,
+          child: Column(
+            children: [
+              const SizedBox(height: 32),
+              Container(
+                margin: const EdgeInsets.fromLTRB(32, 0, 32, 16),
+                width: double.infinity,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: ThemeStyles.theme.accent200,
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                const SizedBox(height: 40),
-                Text(
-                  exception.message ?? "",
-                  style: const TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 0.72),
-                    fontSize: 14,
-                    letterSpacing: 2,
-                    fontFamily: "Loto",
-                    fontWeight: FontWeight.w400,
-                  ),
-                  textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                exception.title ?? "",
+                style: TextStyle(
+                  color: ThemeStyles.theme.accent200,
+                  letterSpacing: 2,
+                  fontSize: 20,
+                  fontFamily: "Loto",
+                  fontWeight: FontWeight.w500,
                 ),
-                const SizedBox(height: 40),
-              ],
-            ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Text(
+                exception.message ?? "",
+                style: TextStyle(
+                  color: ThemeStyles.theme.primary300,
+                  fontSize: 14,
+                  letterSpacing: 2,
+                  fontFamily: "Loto",
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+            ],
           ),
         ),
       ),
