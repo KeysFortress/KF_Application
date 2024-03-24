0bb6247b7c6d1cc306145c4d8fac940ce4446b96
Kristifor Milchev <km@airzen.io>
2024-03-24 15:37:43 +0200

    Implementing certificate uplooads.

--- lib/implementations/certificate_service.dart
+++ lib/implementations/certificate_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:archive/archive.dart';
 import 'package:basic_utils/basic_utils.dart';
 import 'package:flutter/services.dart';
 import 'package:infrastructure/interfaces/icertificate_service.dart';
@@ -60,6 +61,76 @@ class CertificateService implements ICertificateService {
       return true;
     } catch (ex) {
       return false;
+    }
+  }
+
+  @override
+  Future<bool> importCertificate(File file) async {
+    try {
+      var bytes = await file.readAsBytes();
+      final archive = ZipDecoder().decodeBytes(bytes);
+      // For all of the entries in the archive
+      for (var file in archive.files) {
+        // If it's a file and not a directory
+        if (file.isFile) {
+          var data = file.content;
+
+          var fileData = new String.fromCharCodes(data);
+
+          var isCert = isCertificate(fileData);
+          var isPk = isPrivateKey(fileData);
+
+          if (isPk) {
+            await _localStorage.set("key_override", base64.encode(data));
+          }
+
+          if (isCert) {
+            await _localStorage.set(
+              "certificate_override",
+              base64.encode(data),
+            );
+          }
+        }
+      }
+      return true;
+    } catch (ex) {
+      return false;
+    }
+  }
+
+  bool isCertificate(String pemContent) {
+    return pemContent.contains('BEGIN CERTIFICATE') &&
+        pemContent.contains('END CERTIFICATE');
+  }
+
+  bool isPrivateKey(String pemContent) {
+    return pemContent.contains('PRIVATE KEY');
+  }
+
+  @override
+  Future<String?> getKeyPassword() async {
+    return await _localStorage.get("key_override_password");
+  }
+
+  @override
+  Future<bool> setCertificatePassword(String password) async {
+    try {
+      await _localStorage.set("key_override_password", password);
+      return true;
+    } catch (ex) {
+      return false;
+    }
+  }
+
+  @override
+  Future<Map<String, ByteData>> getSystemDefaultIdentities() async {
+    try {
+      var certData = await rootBundle.load(certPath);
+      var keyData = await rootBundle.load(keyPath);
+
+      return {"Certificate": certData, "Key": keyData};
+    } catch (ex) {
+      return {};
     }
   }
 }
